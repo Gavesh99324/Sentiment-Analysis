39de84ec344f06ab15ce0bf0abf9cbb3de789a49
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-11-10 00:07:51 +0530

    Initial commit with preprocessing the data

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Sentiment Analysis',
+    version='0.0.0',
+    author='Gavesh',
+    author_email='gtj99324@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
